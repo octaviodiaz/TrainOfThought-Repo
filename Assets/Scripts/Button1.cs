acb565d3f310b2fb5d15257266054092c8b840c4
Luis Flores <me@luisflores.mx>
2015-06-21 20:44:13 +0000

    Add toggle button

---
+++ Assets/Scripts/Button1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button1 : MonoBehaviour {
+
+	void Update() {
+		Debug.Log ("Update");
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			Debug.Log ("A huevo");
+		}
+	}
+}
