0d944e0285112fc17736ce3edd1469644e54bb8b
octaviodiaz <octavian.diaz@gmail.com>
2015-06-21 16:02:49 -0500

    add click event & collider

--- Assets/Scripts/Button1.cs
+++ Assets/Scripts/Button1.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class Button1 : MonoBehaviour {
 
 	void Update() {
-		Debug.Log ("Update");
+		//Debug.Log ("Update");
 		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
-			Debug.Log ("A huevo");
+			//Debug.Log ("A huevo");
 		}
 	}
+
+
+    void OnMouseDown()
+    {
+        Debug.Log("Click On me");
+    }
 }
