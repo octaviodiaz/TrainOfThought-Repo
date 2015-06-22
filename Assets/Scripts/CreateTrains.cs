385500d2f4e2b93a579138720782fa594f2f1c00
octaviodiaz <octavian.diaz@gmail.com>
2015-06-21 23:02:19 -0500

    add new assets

---
+++ Assets/Scripts/CreateTrains.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using System.Collections;
+
+public class CreateTrains : MonoBehaviour
+{
+
+    private float myTimer = 10.0f;
+    private GameObject blackTrain;
+    private Vector3 blackTrainIPosition;
+    private int blackTrainCounter = 0;
+    private List<GameObject> blackTrains = null;
+
+    void Start()
+    {
+        
+       
+        blackTrain = GameObject.Find("blackTrain");
+        blackTrainIPosition = new Vector3(blackTrain.transform.position.x, blackTrain.transform.position.y, blackTrain.transform.position.z);
+    
+        List<GameObject> blackTrains = new List<GameObject>();
+        for (int i = 0; i < 1000; i++)
+        {
+            blackTrains.Add(GameObject.Find("blackTrain").gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (myTimer > 0)
+        {
+            myTimer -= Time.deltaTime;
+        }
+        if (myTimer <= 0)
+        {
+            myTimer = 10.0f;
+            
+            Instantiate(blackTrains[blackTrainCounter++], blackTrainIPosition, Quaternion.identity);
+            Debug.Log("new black train");
+        
+        }
+    }
+}
+
+
--- Assets/Scripts/MoveTrain.cs
+++ Assets/Scripts/MoveTrain.cs
@@ -1,9 +1,41 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
 public class MoveTrain : MonoBehaviour {
+    
+    private void Start()
+    {
+        try
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+            iTween.MoveTo(gameObject,
+                iTween.Hash("path", iTweenPath.GetPath("TrainPath"), "time", 10, "easytype", iTween.EaseType.easeOutSine));
 
-	void Start(){
-		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("TrainPath"), "time", 25, "easytype", iTween.EaseType.easeOutSine));
-	}
+        }
+        catch (Exception e)
+        {
+            
+            Debug.Log(e.Message);
+        }
+      
+    }
+
+    private void Update()
+    {
+        if (gameObject.GetComponent<Renderer>().enabled == false)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        //gameObject.GetComponent<Renderer>().enabled = false;
+        Debug.Log("black train destroyd");
+    }
+    
+
+
 }
