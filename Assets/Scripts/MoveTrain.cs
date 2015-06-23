9b94c3e3a52a8e0b2b23b18b3ba54cf80afb495d
Luis Flores <me@luisflores.mx>
2015-06-23 00:25:57 +0000

    Add several train paths

--- Assets/Scripts/MoveTrain.cs
+++ Assets/Scripts/MoveTrain.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 public class MoveTrain : MonoBehaviour {
     
     private void Start() {
-        try {
-            gameObject.GetComponent<Renderer>().enabled = false;
-            iTween.MoveTo(gameObject,
-			              iTween.Hash("path", iTweenPath.GetPath("TrainPath"), "time", 10));
-
-        } catch (Exception e) {
-            Debug.Log(e.Message);
-        }
+		moveToPath("TrainPath1");
     }
 
     private void Update() {
@@ -19,9 +12,24 @@ public class MoveTrain : MonoBehaviour {
             gameObject.GetComponent<Renderer>().enabled = true;
         }
 
-    }
-	
+	}
+
+	private void moveToPath(String path) {
+		try {
+			gameObject.GetComponent<Renderer>().enabled = false;
+			iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(path), "time", 10));
+		} catch (Exception e) {
+			Debug.Log(e.Message);
+		}
+	}
+
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("dother" + other.gameObject);
+		if (other.gameObject.name == "Button1") {
+			if (other.gameObject.GetComponent<SpriteRenderer> ().sprite.name == "arrow-left") {
+				moveToPath("TrainPath3");
+			} else {
+				moveToPath("TrainPath2");
+			}
+		}
     }
 }
