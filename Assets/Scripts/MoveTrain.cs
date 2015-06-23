91be1a22a7b3a0f8ec71cd8f8ce081875a94eb81
Luis Flores <me@luisflores.mx>
2015-06-23 00:12:02 +0000

    Add multiple colliders

--- Assets/Scripts/MoveTrain.cs
+++ Assets/Scripts/MoveTrain.cs
@@ -3,39 +3,25 @@ using UnityEngine;
 
 public class MoveTrain : MonoBehaviour {
     
-    private void Start()
-    {
-        try
-        {
+    private void Start() {
+        try {
             gameObject.GetComponent<Renderer>().enabled = false;
             iTween.MoveTo(gameObject,
-                iTween.Hash("path", iTweenPath.GetPath("TrainPath"), "time", 10, "easytype", iTween.EaseType.easeOutSine));
+			              iTween.Hash("path", iTweenPath.GetPath("TrainPath"), "time", 10));
 
-        }
-        catch (Exception e)
-        {
-            
+        } catch (Exception e) {
             Debug.Log(e.Message);
         }
-      
     }
 
-    private void Update()
-    {
-        if (gameObject.GetComponent<Renderer>().enabled == false)
-        {
+    private void Update() {
+        if (gameObject.GetComponent<Renderer>().enabled == false) {
             gameObject.GetComponent<Renderer>().enabled = true;
         }
 
     }
-
-    void OnTriggerEnter(Collider other)
-    {
-        Destroy(gameObject);
-        //gameObject.GetComponent<Renderer>().enabled = false;
-        Debug.Log("black train destroyd");
+	
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("dother" + other.gameObject);
     }
-    
-
-
 }
